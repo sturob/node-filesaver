a6be6f9e782ea8505ce9f35d71cb4c23ff4a66cb
Stuart Robinson <stumrob@gmail.com>
2012-06-29 14:27:46 +0100

    support filepaths, security filter file names + display byte sizes of POSTed files

--- filesaver.js
+++ filesaver.js
@@ -1,53 +1,54 @@
 // #!/usr/bin/node
 
-var app = require('express').createServer(),
-    fs  = require('fs'),
-    port = 6969;
+var app  = require('express').createServer(),
+    fs   = require('fs'),
+    port = 6969,
+    log  = console.log,
+    dirs = {
+      basic:  '/Users/stu/Desktop/browser-data/',
+      vectee: '/Users/stu/Sites/vectee/data/'
+    }
 
-var dirs = {
-  basic:  '/Users/stu/Desktop/browser-data/',
-  vectee: '/Users/stu/Sites/vectee/data/'
-};
+function strip_dotdot(str) { return str.replace(/\.\.\//g, '') }
 
-app.get('/:dir/:file', function(req, res){
-  var p = req.params,
-      file_to_get = dirs[p.dir] + p.file;
+app.get('/:dir/*', function(req, res){
+  var file_to_get = strip_dotdot(dirs[req.params.dir] + req.params[0])
 
-  console.log('GET /' + p.dir + '/' + p.file);
-  console.log('Sending: ' + file_to_get);
+  log( 'GET ' + req.originalUrl )
 
-  var body = fs.readFileSync( file_to_get );
-  res.contentType("text/plain").header('Access-Control-Allow-Origin', '*');
+  var body = fs.readFileSync( file_to_get )
+  res.contentType("text/plain").header('Access-Control-Allow-Origin', '*')
+  log( 'Sending: ' + file_to_get )
   res.send( body );
-});
+})
 
 // ===================================================
 
-app.post('/:dir/:file', function(req, res){
-  var p = req.params,
-      file_to_post = dirs[p.dir] + p.file,
-      body     = '';
+app.post('/:dir/*', function(req, res){
+  var file_to_post = strip_dotdot(dirs[req.params.dir] + req.params[0]),
+      body         = ''
   
+  log( 'POST ' + req.originalUrl )
+
   req.on('data', function (data) {
-    body = body + data;
-  });
+    body += data
+  })
 
   req.on('end', function () {
     fs.writeFile( file_to_post, body, function (err) {
-      if (err) throw err;
-      console.log('Saved ' + file_to_post );
-    });
-  });
+      if (err) throw err
+      log('Saved ' + file_to_post + ' [' + body.length + ']')
+    })
+  })
 
-  console.log('POST to /' + p.dir + '/' + p.file);
-  console.log('Writing to ' + file_to_post );
+  log('Writing to ' + file_to_post)
   
-  res.contentType('text/plain').header('Access-Control-Allow-Origin', '*');
+  res.contentType('text/plain').header('Access-Control-Allow-Origin', '*')
 
-  res.send('gotcha');
-});
+  res.send('gotcha')
+})
 
 // ===================================================
 
-console.log('Listening on: ' + port);
-app.listen(port);
+log('Listening on: ' + port)
+app.listen( port )
