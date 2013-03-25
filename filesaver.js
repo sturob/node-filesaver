621c75a07dc5539849c8ed0f3899500b1c975b91
Stuart Robinson <stumrob@gmail.com>
2013-03-25 20:59:12 +0000

    fixed data directory, less duplicate code + tabbed indentation

--- filesaver.js
+++ filesaver.js
@@ -1,14 +1,9 @@
-// #!/usr/bin/node
-
-var app  = require('express').createServer(),
-    fs   = require('fs'),
-    port = 6969,
-    log  = console.log,
-    dirs = {
-      basic:      '/Users/stu/Desktop/browser-data/',
-      vectee:     '/Users/stu/Desktop/projects/vectee/data/',
-      jollywalker: '/Users/stu/Desktop/projects/thebackofyourhand.com/data/'
-    }
+var express = require('express'),
+    app     = express(),
+    fs      = require('fs'),
+    port    = 6969,
+    log     = console.log,
+    root    = '/Users/stu/Data/',
     red   = '\033[31m',
     green = '\033[32m',
     reset = '\033[0m'
@@ -17,54 +12,58 @@ function strip_dotdot(str) { return str.replace(/\.\.\//g, '') }
 function now() { return (new Date()).toISOString().replace(/T/, ' ').slice(0, -5) }
 
 function ip_allowed(req) {
-  var allowed = (req.ip == '127.0.0.1')
-  if (! allowed) {
-    log('access blocked from ip address: ' + req.ip)
-  }
-  return allowed
+	var allowed = (req.ip == '127.0.0.1')
+	if (! allowed) {
+		log('access blocked from ip address: ' + req.ip)
+	}
+	return allowed
+}
+
+function req_to_file(req) {
+	return strip_dotdot( root + req.params.dir + '/' + req.params[0] );
 }
 
 app.get('/:dir/*', function(req, res) {
-  if (! ip_allowed(req)) return;
-  
-  var file_to_get = strip_dotdot(dirs[req.params.dir] + req.params[0])
+	if (! ip_allowed(req)) return;
+	
+	var file_to_get = req_to_file(req);
 
-  log( 'GET ' + req.originalUrl )
+	log( 'GET ' + req.originalUrl )
 
-  var body = fs.readFileSync( file_to_get )
-  res.contentType("text/plain").header('Access-Control-Allow-Origin', '*')
-  res.header( 'Cache-Control', 'no-cache' )
-  
-  res.send( body )
-  log( green + 'Sent: ' + file_to_get + reset )
+	var body = fs.readFileSync( file_to_get )
+	res.contentType("text/plain").header('Access-Control-Allow-Origin', '*')
+	res.header( 'Cache-Control', 'no-cache' )
+	
+	res.send( body )
+	log( green + 'Sent: ' + file_to_get + reset )
 })
 
 // ===================================================
 
 app.post('/:dir/*', function(req, res) {
-  if (! ip_allowed(req)) return;
+	if (! ip_allowed(req)) return;
 
-  var file_to_post = strip_dotdot(dirs[req.params.dir] + req.params[0]),
-      body         = ''
-  
-  log( 'POST ' + req.originalUrl )
+	var file_to_post = req_to_file(req),
+			body         = '';
+	
+	log( 'POST ' + req.originalUrl )
 
-  req.on('data', function (data) {
-    body += data
-  })
+	req.on('data', function (data) {
+		body += data
+	})
 
-  req.on('end', function () {
-    fs.writeFile( file_to_post, body, function (err) {
-      if (err) throw err
-      log(now() + green + ' Saved ' + file_to_post + reset + ' ' + body.length )
-    })
-  })
+	req.on('end', function () {
+		fs.writeFile( file_to_post, body, function (err) {
+			if (err) throw err
+			log(now() + green + ' Saved ' + file_to_post + reset + ' ' + body.length )
+		})
+	})
 
-  log( 'Writing to ' + file_to_post)
-  
-  res.contentType('text/plain').header('Access-Control-Allow-Origin', '*')
+	log('Writing to ' + file_to_post)
+	
+	res.contentType('text/plain').header('Access-Control-Allow-Origin', '*')
 
-  res.send('gotcha')
+	res.send('gotcha\n')
 })
 
 // ===================================================
